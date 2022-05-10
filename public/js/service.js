41c6bb40a4371aaaccd248578673dbb50a4925d1
Andre Luiz <and969696@outlook.com>
2022-05-10 14:50:31 -0300

    agora vai

--- public/js/service.js
+++ public/js/service.js
@@ -90,7 +90,7 @@ function submitForm(event) {
         tamanho_camisa: getValue('tamanho_camisa'),
     }
 
-    fetch('https://comejaca-p2gqs3be0-andrelduarte.vercel.app/', {
+    fetch('https://comejaca-app.vercel.app/api/inscricoes', {
         headers: {
             "Content-Type": "application/json"
         },
