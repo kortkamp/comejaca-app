26cfdd4f4cf41e9ea2ddc99f631a3b50b8d5fe15
Andre Luiz <and969696@outlook.com>
2022-05-16 16:47:22 -0300

    change fetch in https://www.comejaca.org.br/

--- public/js/service.js
+++ public/js/service.js
@@ -90,7 +90,7 @@ function submitForm(event) {
         tamanho_camisa: getValue('tamanho_camisa'),
     }
 
-    fetch('https://comejaca.org.br/api/inscricoes/', {
+    fetch('https://www.comejaca.org.br/api/inscricoes/', {
         headers: {
             "Content-Type": "application/json"
         },
